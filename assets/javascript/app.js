cf487aef83601a92790e3f3cabdcff863b8c395c
David Koeller <david.koeller@gmail.com>
2020-01-26 21:20:22 -0600

    the js works, needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,12 +8,90 @@
     storageBucket: "train-scheduler-9bbf9.appspot.com",
     messagingSenderId: "677304708482",
     appId: "1:677304708482:web:c88f2d0d41e853b7b8739b",
-    measurementId: "G-99H7HJK4GR"
+    
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+  
 
 
 //database reference
 var database = firebase.database();
+
+$("#add-train-btn").on("click", function(event) {
+  event.preventDefault();
+
+  var trainName = $("#train-name-input").val().trim();
+
+  var trainDestination = $("#destination-input").val().trim();
+
+  var trainStart = $("#train-start-input").val().trim();
+
+  var trainFreq = $("#frequency-input").val().trim();
+
+  
+
+  var newTrain = {
+    train: trainName,
+    destination: trainDestination,
+    trainStart: trainStart,
+    frequency: trainFreq
+
+  };
+
+  database.ref().push(newTrain);
+
+  //clear the text boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#train-start-input").val("");
+  $("#frequency-input").val("");
+
+
+});
+
+
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+  //variables for the stored data
+  var trainName = childSnapshot.val().train;
+
+  var trainDestination = childSnapshot.val().destination;
+
+  var trainStart = childSnapshot.val().trainStart;
+
+  var trainFreq = childSnapshot.val().frequency;
+  //LOTs of fun math!
+  //
+  // var trainStartConvert = moment(trainStart, "HH:mm");
+  // console.log(trainStartConvert);
+  //
+  var timeDiff = moment().diff(moment(moment(trainStart, "HH:mm")), "minutes");
+
+  var remainder = timeDiff % trainFreq;
+  
+  var timeTill = trainFreq - remainder;
+
+  var arrivalTime = moment().add(timeTill, "minutes");
+ 
+  var arrivalConvert = moment(arrivalTime).format("hh:mm A");
+
+  if (timeTill === 1) {
+    timeTill = "This Train is About to Arrived";
+  }
+  
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDestination),
+    $("<td>").text(trainFreq),
+    $("<td>").text(arrivalConvert),
+    $("<td>").text(timeTill)
+  );
+
+  $("#train-table > tbody").append(newRow);
+  
+});
+  
+
+  
+  
